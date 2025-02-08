deccb88132d4bb85c9eeec1810a84158ecf254fc
hashtagp <hashtag.p27@gmail.com>
2025-02-08 11:57:43 +0530

    vercel.json

--- api/server.js
+++ api/server.js
@@ -66,4 +66,8 @@ app.get('/',(req,res)=>{
 
 // Start the Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app; //for vercel
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
